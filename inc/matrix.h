0e442e0c6d04df1fd9b7077fe09030139af358f4
ATmega8 <liuyang.2014@foxmail.com>
2016-05-05 08:56:46 +0000

    矩阵创建与元素读取函数

---
+++ inc/matrix.h
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+#define MATRIX_DATATYPE float
+
+typedef struct
+{
+	uint32_t           rowCount;
+	uint32_t		   colCount;
+	MATRIX_DATATYPE*   pdata;
+} MATRIX_TypeDef;
+
+typedef enum
+{
+	MartixNoTrans,
+	MatrixTrans
+} MATRIX_TRANS_TypeDef;
+
+MATRIX_TypeDef* MATRIX_Create(uint32_t row, uint32_t col);
+void MATRIX_Destroy(MATRIX_TypeDef* matrix);
+
+extern void MATRIX_SetData(MATRIX_TypeDef* matrix,
+		const uint32_t row, const uint32_t col, const MATRIX_DATATYPE data);
+
+extern MATRIX_DATATYPE MATRIX_GetData(const MATRIX_TypeDef* matrix,
+		const uint32_t row, const uint32_t col);
+
+void MATRIX_Add(MATRIX_TypeDef* A, const MATRIX_TypeDef* B);
+
+#define MATRIX_ASSERT(expr) ((expr) ? (void)0 : MATRIX_AssertFailed(__FILE__, __LINE__))
+void MATRIX_AssertFailed(const char* file, uint32_t line);
+
